b26d3d1f8d278703e1c0e7b0d72c900937b0a2d2
Dimitrios Kalligaridis <dimitriskalligaridis@gmail.com>
2026-07-04 19:44:56 +0300

    Add base template and home page

--- argus/urls.py
+++ argus/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('catalog.urls')),
 ]
 
 # Serve uploaded images from the media folder while developing.
