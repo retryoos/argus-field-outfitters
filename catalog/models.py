0e25af637d6209affc05a45360064722dbad02bb
Dimitrios Kalligaridis <dimitriskalligaridis@gmail.com>
2026-07-05 12:35:51 +0300

    Add Category and Subcategory models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'subcategories'
+
+    def __str__(self):
+        return self.name
