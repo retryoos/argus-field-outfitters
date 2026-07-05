b23a792fec99ffdb7fdae0ebe053cccb5b634ff2
Dimitrios Kalligaridis <dimitriskalligaridis@gmail.com>
2026-07-05 13:23:34 +0300

    Register catalogue models in the admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Category, Subcategory, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ['name']}
+    search_fields = ['name']
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    list_filter = ['category']
+    prepopulated_fields = {'slug': ['name']}
+    search_fields = ['name']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'subcategory', 'price', 'stock', 'thumbnail']
+    list_filter = ['subcategory', 'brand', 'color']
+    search_fields = ['name', 'brand']
+    list_editable = ['price', 'stock']
+
+    def thumbnail(self, product):
+        if product.image:
+            return format_html('<img src="{}" style="height: 40px;">', product.image.url)
+        return 'No image'
+    thumbnail.short_description = 'Image'
